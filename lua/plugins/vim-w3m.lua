3acb44256527ce4840b942760b092c7d76140491
126282728+bescka@users.noreply.github.com <126282728+bescka@users.noreply.github.com>
2024-11-17 14:45:42 +0000

    package: disable w3m

--- lua/plugins/vim-w3m.lua
+++ lua/plugins/vim-w3m.lua
@@ -1,5 +1,6 @@
 return {
         "yuratomo/w3m.vim",
+        enabled = false,
         config = function()
             -- Optional: Key mappings for vim-w3m
             vim.api.nvim_set_keymap('n', '<Leader>ob', ':W3M<CR>', { noremap = true, silent = true })
